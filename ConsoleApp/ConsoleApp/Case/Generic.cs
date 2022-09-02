626c9665304e7b1e1850870e162eb68cb352be2c
xuhaixia <xuhaixia200@163.com>
2022-09-02 22:58:55 +0800

    泛型

---
+++ ConsoleApp/ConsoleApp/Case/Generic.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp.Case
+{
+    /// <summary>
+    /// 泛型
+    /// </summary>
+    public class Generic
+    {
+        
+    }
+    /// <summary>
+    /// 声明一个泛型类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+
+    public class GenericList<T>
+    {
+        public void Add(T input) { }
+    }
+
+    public class TestGenericList
+    {
+        private class ExampleClass { }
+        static void Main()
+        {
+            //声明一个int类型列表
+            GenericList<int> list1 = new GenericList<int>();
+            list1.Add(1);
+
+            //声明一个字符串类型列表
+            GenericList<string> list2=new GenericList<string>();
+            list2.Add("");
+
+            //声明一个ExampleClass类型列表
+            GenericList<ExampleClass> list=new GenericList<ExampleClass>();
+            list.Add(new ExampleClass());
+        }
+    }
+}
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            string message = null;
+            //警告:零废弃。
+            Console.WriteLine($"消息的长度为: {message.Length}");
+
+            var originalMessage = message;
+            message = "Hello World!";
+
+            //没有警告。分析确定“消息”不是空的。
+            Console.WriteLine($"消息的长度为: {message.Length}");
+
+            //警告
+            Console.WriteLine(originalMessage.Length);
+
+
+            Console.ReadLine();
         }
     }
 }
